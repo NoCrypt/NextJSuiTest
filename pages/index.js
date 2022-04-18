7b97647d741553e8a2a5b47d419eaf3085c4c5fe
NoCrypt <aldyhkmz@gmail.com>
2022-04-19 05:28:25 +0700

    NextUI Installed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head';
+import Button from '@nextui-org/react/button';
+
+const Component = () => <Button>Click me</Button>;
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <Component />;
 }
