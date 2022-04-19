66075e09e2292ae2759d7457d7a5b6e157721254
NoCrypt <aldyhkmz@gmail.com>
2022-04-19 08:00:23 +0700

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 import Head from 'next/head';
 import Button from '@nextui-org/react/button';
-import { useTheme, Text } from '@nextui-org/react';
+import { useTheme as useNextTheme } from 'next-themes';
+import { Switch, useTheme } from '@nextui-org/react';
+
+const App = () => {
+  const { setTheme } = useNextTheme();
+  const { isDark, type } = useTheme();
+
+  return (
+    <div>
+      The current theme is: {type}
+      <Switch
+        checked={isDark}
+        onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
+      />
+    </div>
+  );
+};
 
 export default function Home() {
-  return <></>;
+  return <App />;
 }
