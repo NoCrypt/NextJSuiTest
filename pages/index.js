44157f1eaa075c8ff4a62073c01ab892723c83ae
NoCrypt <aldyhkmz@gmail.com>
2022-04-20 15:13:55 +0700

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import Head from 'next/head';
 import Button from '@nextui-org/react/button';
 import { useTheme as useNextTheme } from 'next-themes';
-import { Switch, useTheme, Text, Container, Row, css } from '@nextui-org/react';
+import {
+  Switch,
+  useTheme,
+  Text,
+  Container,
+  Input,
+  Grid,
+} from '@nextui-org/react';
 
 const App = () => {
   const { setTheme } = useNextTheme();
@@ -21,23 +28,38 @@ export default function Home() {
   return (
     <Container
       fluid
-      justifyContent="center"
-      alignItems="center"
+      w="100%"
+      display="flex"
+      direction="column"
+      justify="center"
+      alignItems="middle"
       css={{
         minHeight: '100vh',
-        border: '$space$1 solid $blue500',
       }}
     >
       <Text
         h1
-        size={90}
         css={{
           textGradient: '45deg, $blue500 -20%, $pink500 50%',
-          border: '$space$1 solid $blue500',
+          fontWeight: '700',
+          fontSize: '40',
+          pb: '$10',
+          '@sm': {
+            fontSize: '60',
+          },
         }}
       >
-        Form NextUI.
+        Form NextUI
       </Text>
+      <Grid.Container gap={4} justify="center">
+        <Grid>
+          <Input clearable bordered labelPlaceholder="Name" color="primary" />
+        </Grid>
+        <Grid>
+          <Input clearable bordered labelPlaceholder="Name" color="primary" />
+        </Grid>
+      </Grid.Container>
+      <App />
     </Container>
   );
 }
