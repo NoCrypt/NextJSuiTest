403fa7808936fd2482e97aaa083afe654978ac59
NoCrypt <aldyhkmz@gmail.com>
2022-04-20 10:59:26 +0700

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Button from '@nextui-org/react/button';
 import { useTheme as useNextTheme } from 'next-themes';
-import { Switch, useTheme } from '@nextui-org/react';
+import { Switch, useTheme, Text, Container, Row, css } from '@nextui-org/react';
 
 const App = () => {
   const { setTheme } = useNextTheme();
@@ -18,5 +18,26 @@ const App = () => {
 };
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <Container
+      fluid
+      justifyContent="center"
+      alignItems="center"
+      css={{
+        minHeight: '100vh',
+        border: '$space$1 solid $blue500',
+      }}
+    >
+      <Text
+        h1
+        size={90}
+        css={{
+          textGradient: '45deg, $blue500 -20%, $pink500 50%',
+          border: '$space$1 solid $blue500',
+        }}
+      >
+        Form NextUI.
+      </Text>
+    </Container>
+  );
 }
